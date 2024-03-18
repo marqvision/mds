bf0d5c4cfd33235fad6239eabfedf64c4f3d5825
Eddie <eddie.jang@marqvision.com>
2024-03-18 21:22:00 +0900

    feat(mds): [PROD-8571] add generic type for MDSSelectContainer props

--- components/SelectContainer/@types.ts
+++ components/SelectContainer/@types.ts
@@ -1,18 +1,30 @@
-import { HTMLAttributes, ReactElement } from 'react';
+import { HTMLAttributes, ReactElement, ReactNode } from 'react';
+import { Path } from '../../@system/types';
+import { MDSTheme } from '../../foundation';
 
 // Figma https://www.figma.com/file/3UJ0kZhf62nuG9erDNzLc6/MDS?type=design&node-id=8140-3261&mode=design&t=WFZGdzrmqN5f7Llc-0
 
-export type Features = {
-  title?: string;
-  /**
-   * Select Container Main Icon
-   */
-  mainIcon?: ReactElement;
-  /**
-   * Title 왼쪽에 가는 Icon
-   */
-  titleIcon?: ReactElement;
+export type ContainerFeatures<T> = {
+  selectedValue: string | string[];
+  onClick: (value: T) => void;
+  children: ReactElement | ReactElement[];
+};
+
+export type ItemFeatures = {
+  main?: {
+    icon: ReactNode;
+    color: Path<Pick<MDSTheme, 'color'>>;
+  };
+  title: {
+    label: string;
+    icon?: ReactNode;
+    color?: Path<Pick<MDSTheme, 'color'>>;
+  };
+  isSelected?: boolean;
+  value: string;
+  content: ReactNode;
   disabled?: boolean;
 };
 
-export type MDSSelectContainerProps = Features & HTMLAttributes<HTMLDivElement>;
+export type MDSSelectContainerProps<T = unknown> = ContainerFeatures<T> & HTMLAttributes<HTMLDivElement>;
+export type MDSSelectContainerItemProps = ItemFeatures;

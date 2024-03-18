0498cb9df4ef6a650d8eb03bd546e48326dcec97
Eddie <eddie.jang@marqvision.com>
2024-03-18 21:23:10 +0900

    feat(mds): [PROD-8571] [WIP] add styles for different MDSSelectContainer variant and add basic skeleton model of MDSSelectContainer

--- components/SelectContainer/index.tsx
+++ components/SelectContainer/index.tsx
@@ -1,6 +1,199 @@
-import { MDSSelectContainerProps } from './@types';
+import { cloneElement, Children, ReactElement } from 'react';
+import clsx from 'clsx';
+import styled from '@emotion/styled';
+import { IcoCheckFill } from '@marqvision/mds/assets';
+import { MDSTypography } from '../Typography';
+import { MDSThemeValue } from '../../foundation';
+import { MDSSelectContainerProps, MDSSelectContainerItemProps } from './@types';
 
-// Ticket:
-export const MDSSelectContainer = ({ title = undefined, ...props }: MDSSelectContainerProps) => {
-  return <div>Add logic here</div>;
+const SelectContainerStyles = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+const SelectContainerTitleStyles = styled.div`
+  display: flex;
+`;
+
+const SelectContainerItemContentStyles = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+`;
+const MainIconWrapperStyles = styled.div`
+  width: 24px;
+  height: 24px;
+`;
+
+const CheckedIconWrapperStyles = styled.div`
+  position: absolute;
+  top: -9px;
+  left: -9px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 18px;
+  height: 18px;
+  border-radius: 100px;
+  background-color: ${MDSThemeValue.color.bg.surface.neutral.default.normal};
+
+  & path {
+    width: 16px;
+    height: 16px;
+  }
+`;
+
+const SelectContainerItemStyles = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  gap: 16px;
+  padding: 12px 16px;
+  border-radius: 8px;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+
+  &.isNotSelected {
+    background-color: ${MDSThemeValue.color.bg.surface.neutral.default.normal};
+    outline: 1px solid ${MDSThemeValue.color.border.neutral.default.normal};
+
+    .checked-icon-wrapper path {
+      fill: ${MDSThemeValue.color.content.primary.default.normal};
+    }
+
+    &:hover {
+      background-color: ${MDSThemeValue.color.bg.surface.neutral.default.hover};
+
+      .checked-icon-wrapper path {
+        fill: ${MDSThemeValue.color.content.primary.default.hover};
+      }
+    }
+  }
+
+  &.isSelected {
+    background-color: ${MDSThemeValue.color.bg.surface.neutral.default.normal};
+    outline: 2px solid ${MDSThemeValue.color.border.primary.default.normal};
+
+    .checked-icon-wrapper path {
+      fill: ${MDSThemeValue.color.content.primary.default.normal};
+    }
+
+    &:hover {
+      background-color: ${MDSThemeValue.color.bg.surface.neutral.default.hover};
+
+      .checked-icon-wrapper path {
+        fill: ${MDSThemeValue.color.content.primary.default.hover};
+      }
+    }
+  }
+
+  &.disabled {
+    outline: 1px solid ${MDSThemeValue.color.border.neutral.default.normal};
+
+    p {
+      color: ${MDSThemeValue.color.content.neutral.default.disabled};
+    }
+
+    .checked-icon-wrapper path {
+      fill: ${MDSThemeValue.color.content.primary.default.disabled};
+    }
+
+    &:hover {
+      background-color: ${MDSThemeValue.color.bg.surface.neutral.default.disabled};
+    }
+  }
+
+  &.disabledAndSelected {
+    outline: 2px solid ${MDSThemeValue.color.border.primary.default.disabled};
+
+    p {
+      color: ${MDSThemeValue.color.content.neutral.default.disabled};
+    }
+
+    .checked-icon-wrapper path {
+      fill: ${MDSThemeValue.color.content.primary.default.disabled};
+    }
+
+    &:hover {
+      background-color: ${MDSThemeValue.color.bg.surface.neutral.default.disabled};
+
+      .checked-icon-wrapper path {
+        fill: ${MDSThemeValue.color.content.primary.default.disabled};
+      }
+    }
+  }
+`;
+
+export const MDSSelectContainer = ({ onClick, selectedValue, children }: MDSSelectContainerProps) => {
+  const modifiedChildrenWithProps = Children.map(children, (child: ReactElement<any>) => {
+    const childValueProps = child.props.value;
+    const childDisabledProps = child.props.disabled;
+
+    // Compare if selectedValue is string type or array type
+    if (typeof selectedValue === 'string') {
+      //TODO: add condition to disable click event in disabled list (eddie)
+      if (selectedValue === childValueProps) {
+        return cloneElement(child, {
+          isSelected: true,
+          onClick: () => onClick(childValueProps),
+        });
+      } else {
+        return cloneElement(child, {
+          isSelected: false,
+          onClick: () => onClick(childValueProps),
+        });
+      }
+    } else {
+      if (selectedValue?.includes(childValueProps)) {
+        return cloneElement(child, {
+          isSelected: true,
+          onClick: () => onClick(childValueProps),
+        });
+      } else {
+        return cloneElement(child, {
+          isSelected: false,
+          onClick: () => onClick(childValueProps),
+        });
+      }
+    }
+  });
+
+  return <SelectContainerStyles>{modifiedChildrenWithProps}</SelectContainerStyles>;
+};
+
+export const MDSSelectContainerItem = ({
+  main,
+  title,
+  isSelected,
+  disabled,
+  content,
+  ...props
+}: MDSSelectContainerItemProps) => {
+  const className = clsx({
+    isSelected: isSelected,
+    isNotSelected: !isSelected,
+    disabled: disabled,
+    disabledAndSelected: disabled && isSelected,
+  });
+
+  return (
+    <SelectContainerItemStyles className={className} {...props}>
+      {isSelected && (
+        <CheckedIconWrapperStyles className="checked-icon-wrapper">
+          <IcoCheckFill className="check-icon" style={{ width: '16px' }} />
+        </CheckedIconWrapperStyles>
+      )}
+      {main?.icon && <MainIconWrapperStyles>{main.icon}</MainIconWrapperStyles>}
+      <SelectContainerItemContentStyles>
+        <SelectContainerTitleStyles>
+          {title.icon}
+          <MDSTypography variant="T14" weight="medium" color={title.color} style={{ width: 'max-content' }}>
+            {title.label}
+          </MDSTypography>
+        </SelectContainerTitleStyles>
+        {content}
+      </SelectContainerItemContentStyles>
+    </SelectContainerItemStyles>
+  );
 };

bde14a207ea144334b4dd69c78b4a0cb65f7b4c9
Eddie <eddie.jang@marqvision.com>
2024-03-20 10:27:44 +0900

    fix(mds): [PROD-8571] add hover style border on selected container

--- components/SelectContainer/@utils.ts
+++ components/SelectContainer/@utils.ts
@@ -41,10 +41,10 @@ export const getCorrectContainerStyle = (disabled?: boolean, isSelected?: boolea
     `;
   }
 
-  if (!isSelected) {
+  if (isSelected) {
     return `
       background-color: ${MDSThemeValue.color.bg.surface.neutral.default.normal};
-      outline: 1px solid ${MDSThemeValue.color.border.neutral.default.normal};
+      outline: 2px solid ${MDSThemeValue.color.border.primary.default.normal};
   
       .checked-icon-wrapper path {
         fill: ${MDSThemeValue.color.content.primary.default.normal};
@@ -52,7 +52,8 @@ export const getCorrectContainerStyle = (disabled?: boolean, isSelected?: boolea
   
       &:hover {
         background-color: ${MDSThemeValue.color.bg.surface.neutral.default.hover};
-  
+        outline: 2px solid ${MDSThemeValue.color.border.primary.default.hover};
+        
         .checked-icon-wrapper path {
           fill: ${MDSThemeValue.color.content.primary.default.hover};
         }
@@ -60,10 +61,10 @@ export const getCorrectContainerStyle = (disabled?: boolean, isSelected?: boolea
     `;
   }
 
-  if (isSelected) {
+  if (!isSelected) {
     return `
       background-color: ${MDSThemeValue.color.bg.surface.neutral.default.normal};
-      outline: 2px solid ${MDSThemeValue.color.border.primary.default.normal};
+      outline: 1px solid ${MDSThemeValue.color.border.neutral.default.normal};
   
       .checked-icon-wrapper path {
         fill: ${MDSThemeValue.color.content.primary.default.normal};

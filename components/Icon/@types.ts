789bb901bb61de89b481058f1eddedc6dd7f40e0
jamie <jamie.cho@marqvision.com>
2024-03-22 18:19:10 +0900

    feat(mds-v2): [PROD-7855] add new icons

--- components/Icon/@types.ts
+++ components/Icon/@types.ts
@@ -1,7 +1,16 @@
 import { Path } from '../../@system/types';
 import type { MDSTheme } from '../../foundation';
 
-export type MDSIconProps = {
-  size: number;
-  color?: Path<Pick<MDSTheme,'color'>>;
+export type Features = {
+  /**
+   * 아이콘의 크기.
+   * 아이콘은 정사각형이므로 width와 height가 동일합니다.
+   */
+  size?: number;
+  color?: Path<Pick<MDSTheme, 'color'>>;
+  /**
+   * 아이콘의 스타일 종류.
+   * 일부 아이콘은 해당 속성을 지원하지 않습니다.
+   */
+  variant?: 'outline' | 'border' | 'fill';
 };

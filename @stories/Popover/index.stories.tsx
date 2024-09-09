244320274358ce8060b99684a3ddb1a30a111145
matthew <matthew.lee@marqvision.com>
2024-09-09 18:00:07 +0900

    feat(mds): [PROD-10856] add MDSPopover stories

---
+++ @stories/Popover/index.stories.tsx
@@ -0,0 +1,103 @@
+import React, { useState } from 'react';
+import { useArgs } from '@storybook/preview-api';
+import { MDSPopover, MDSRadioButton, MDSTypography } from '../../components';
+import type { Meta, StoryFn, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof MDSPopover> = {
+  component: MDSPopover,
+  title: '2. Components/Popover',
+  args: {
+    anchor: <button>클릭</button>,
+  },
+  parameters: {
+    layout: 'center',
+  },
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof MDSPopover>;
+
+const Wrapper = ({ children }: React.PropsWithChildren) => {
+  return (
+    <div
+      style={{
+        padding: '24px',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '24px',
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+export const Preview: Story = {
+  args: {
+    anchor: <button style={{ width: '200px' }}>클릭</button>,
+  },
+  render: function Render(props) {
+    return (
+      <Wrapper>
+        <MDSPopover {...props}>
+          <MDSTypography>팝업</MDSTypography>
+        </MDSPopover>
+      </Wrapper>
+    );
+  },
+};
+
+export const Hover: Story = {
+  render: function Render() {
+    return (
+      <Wrapper>
+        <MDSPopover anchor={<button style={{ width: '200px' }}>마우스 호버</button>} hasDim={false} trigger="hover">
+          <MDSTypography>팝업</MDSTypography>
+        </MDSPopover>
+      </Wrapper>
+    );
+  },
+};
+
+export const CustomEvent: Story = {
+  render: function Render() {
+    const [isLoading, setIsLoading] = useState(false);
+    const [data, setData] = useState<number>();
+
+    const handleOpen = () => {
+      setIsLoading(true);
+
+      setTimeout(() => {
+        setIsLoading(false);
+        setData(10);
+      }, 2000);
+    };
+
+    return (
+      <Wrapper>
+        <MDSPopover
+          anchor={({ open }) => (
+            <button
+              style={{ width: '200px' }}
+              onClick={(e) => {
+                handleOpen();
+                open(e);
+              }}
+            >
+              클릭
+            </button>
+          )}
+          isLoading={isLoading}
+        >
+          {({ close }) => (
+            <>
+              <MDSTypography>팝업 {data}</MDSTypography>
+              <button onClick={close}>닫기</button>
+            </>
+          )}
+        </MDSPopover>
+      </Wrapper>
+    );
+  },
+};

3ea651053d07fc2be972a3d08a4990a501c94bf5
Morgan.lee <morgan.lee@marqvision.com>
2024-11-19 11:25:57 +0900

    fix(shared): td, th, col reset 방지

--- foundation/resetCSS.tsx
+++ foundation/resetCSS.tsx
@@ -14,7 +14,9 @@ export const MDSResetCSS = () => (
         - The "html" element is excluded, otherwise a bug in Chrome breaks the CSS hyphens property (https://github.com/elad2412/the-new-css-reset/issues/36)
       */
 
-      *:where(:not(html, iframe, canvas, img, svg, video, audio, button, dialog, table, tr, td):not(svg *, symbol *)) {
+      *:where(
+          :not(html, iframe, canvas, img, svg, video, audio, button, dialog, table, td, th, col):not(svg *, symbol *)
+        ) {
         all: unset;
         display: revert;
       }

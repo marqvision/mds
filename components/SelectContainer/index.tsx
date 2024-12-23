38788418a0864fb3d5b2cc5e78e59d488f6ac717
Morgan.lee <morgan.lee@marqvision.com>
2024-12-23 16:18:03 +0900

    fix(mds): [PROD-12360] flex wrap 추가 (item 갯수 많아지면 줄바꿈 필요)

--- components/SelectContainer/index.tsx
+++ components/SelectContainer/index.tsx
@@ -14,6 +14,7 @@ import { getCorrectContainerStyle, getLayoutStyle } from './@utils';
 const SelectContainerStyles = styled.div<StyledWrapperProps>`
   display: flex;
   flex-direction: ${({ orientation }) => (orientation === 'horizontal' ? 'row' : 'column')};
+  flex-wrap: wrap;
   gap: 8px;
 `;
 

90f89b218f9e227408a9cfa42206748efeab7bd6
Morgan.lee <morgan.lee@marqvision.com>
2025-05-11 02:21:24 +0900

    fix(mds): [PROD-14706] console warning 대응

--- components/organisms/LNB/@components/StubLink.tsx
+++ components/organisms/LNB/@components/StubLink.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { MouseEvent } from 'react';
 
 type Props = {
   to: string;
   children: React.ReactNode;
-} & React.RefAttributes<HTMLAnchorElement>;
+  className?: string;
+  onMouseEnter?: (event: MouseEvent) => void;
+  onMouseLeave?: (event: MouseEvent) => void;
+};
 
 /*
  * LinkComponent 를 주입하지 않았을 때 출력될 대체 컴포넌트
  */
 export const StubLink = (props: Props) => {
-  const { to, children, ...restProps } = props;
+  const { to, children, className, onMouseEnter, onMouseLeave } = props;
 
   return (
-    <a href={to} {...restProps}>
+    <a href={to} className={className} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
       {children}
     </a>
   );

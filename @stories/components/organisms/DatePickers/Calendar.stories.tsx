bc2c8d75443198bc5ce9a1ae0dacab7aaa565d27
jamie <jamie.cho@marqvision.com>
2025-06-20 17:22:37 +0900

    chore(mds-v2): [PROD-10004] Calendar - date range, 기본값 없는 스토리 추가

--- @stories/components/organisms/DatePickers/Calendar.stories.tsx
+++ @stories/components/organisms/DatePickers/Calendar.stories.tsx
@@ -1,8 +1,9 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { useState } from '@storybook/preview-api';
 import dayjs from 'dayjs';
+import { css } from '@emotion/react';
 import { MDSCalendar } from '../../../../components/organisms/DatePickers/Calendar';
-import { MDSButton, MDSInput, MDSTypography } from '../../../../components';
+import { MDSInput, MDSTypography } from '../../../../components';
 
 const meta: Meta<typeof MDSCalendar> = {
   component: MDSCalendar,
@@ -29,8 +30,30 @@ const meta: Meta<typeof MDSCalendar> = {
 export default meta;
 type Story = StoryObj<typeof MDSCalendar>;
 
+const testGroupStyle = css`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
+const dataBoxLayout = css`
+  display: flex;
+  gap: 24px;
+`;
+const dataBoxStyle = css`
+  border: 1px solid lightgray;
+  padding: 20px;
+  border-radius: 10px;
+  width: 304px;
+  display: grid;
+  gap: 12px;
+`;
+
 export const SingleDate: Story = {
   render: function Render() {
+    const DEFAULT_VALUE = {
+      format: 'MM/DD/YYYY',
+    };
+
     const [selectedDate, setSelectedDate] = useState<Date | undefined>(undefined);
     const handleChange = (newDate: Date) => {
       setSelectedDate(newDate);
@@ -38,71 +61,118 @@ export const SingleDate: Story = {
 
     const [inputValue, setInputValue] = useState<string>(selectedDate?.toLocaleDateString() || '');
     const submitInputChange = (value: string) => {
-      const newDate = dayjs(value, 'MM/DD/YYYY').toDate();
+      const newDate = dayjs(value, DEFAULT_VALUE.format).toDate();
       setSelectedDate(newDate);
     };
     return (
-      <div>
-        <div>
-          <MDSTypography>선택한 날짜: {selectedDate?.toLocaleDateString()}</MDSTypography>
+      <div css={testGroupStyle}>
+        <MDSTypography variant="title">Case 1: 단일 날짜 선택</MDSTypography>
+
+        <div css={dataBoxLayout}>
+          <div css={dataBoxStyle}>
+            <MDSTypography variant="title" size="m">
+              외부 값 주입을 위한 컴포넌트
+            </MDSTypography>
+            <MDSInput
+              fullWidth
+              variant="textField"
+              value={inputValue}
+              placeholder={DEFAULT_VALUE.format}
+              custom={{
+                add: {
+                  label: '선택',
+                  onSubmit: submitInputChange,
+                },
+              }}
+              onChange={setInputValue}
+            />
+          </div>
+          <div css={dataBoxStyle}>
+            <MDSTypography variant="title" size="m">
+              props
+            </MDSTypography>
+            <div>
+              <pre>{JSON.stringify(DEFAULT_VALUE, null, 2)}</pre>
+            </div>
+          </div>
+          <div css={dataBoxStyle}>
+            <MDSTypography variant="title" size="m">
+              결과
+            </MDSTypography>
+            <MDSTypography>선택한 날짜: {selectedDate?.toLocaleDateString()}</MDSTypography>
+          </div>
         </div>
         <div>
-          <MDSInput
-            variant="textField"
-            value={inputValue}
-            placeholder="MM/DD/YYYY"
-            custom={{
-              add: {
-                label: '선택',
-                onSubmit: submitInputChange,
-              },
-            }}
-            onChange={setInputValue}
-          />
+          <MDSCalendar value={selectedDate} onChange={handleChange} />
         </div>
-        <MDSCalendar value={selectedDate} onChange={handleChange} />
       </div>
     );
   },
 };
 export const SingleDateWithMinMaxAndDefaultValue: Story = {
   render: function Render() {
-    const args = {
-      value: new Date(),
-      minDate: dayjs('2023-11-22').toDate(),
-      maxDate: dayjs('2027-11-11').toDate(),
+    const DEFAULT_VALUE = {
+      format: 'MM/DD/YYYY',
+      minDate: '11/22/2023',
+      maxDate: '11/22/2027',
+      value: '06/20/2025',
     };
-    const [selectedDate, setSelectedDate] = useState<Date>(args.value as Date);
+
+    const [selectedDate, setSelectedDate] = useState<Date>(new Date());
     const handleChange = (newDate: Date) => {
       setSelectedDate(newDate);
     };
 
-    const [inputValue, setInputValue] = useState<string>(selectedDate.toLocaleDateString());
+    const [inputValue, setInputValue] = useState<string>(dayjs(selectedDate).format(DEFAULT_VALUE.format));
     const submitInputChange = (value: string) => {
-      const newDate = dayjs(value, 'MM/DD/YYYY').toDate();
+      const newDate = dayjs(value, DEFAULT_VALUE.format).toDate();
       setSelectedDate(newDate);
     };
     return (
-      <div>
-        <div>
-          <MDSTypography>minDate: {args.minDate.toLocaleDateString()}</MDSTypography>
-          <MDSTypography>maxDate: {args.maxDate.toLocaleDateString()}</MDSTypography>
-          <MDSTypography>선택한 날짜: {selectedDate?.toLocaleDateString()}</MDSTypography>
+      <div css={testGroupStyle}>
+        <MDSTypography variant="title">Case 2: 단일 날짜 + min/max + 기본값</MDSTypography>
+        <div css={dataBoxLayout}>
+          <div css={dataBoxStyle}>
+            <MDSTypography variant="title" size="m">
+              외부 값 주입을 위한 컴포넌트
+            </MDSTypography>
+            <MDSInput
+              fullWidth
+              variant="textField"
+              value={inputValue}
+              placeholder={DEFAULT_VALUE.format}
+              custom={{
+                add: {
+                  label: '선택',
+                  onSubmit: submitInputChange,
+                },
+              }}
+              onChange={setInputValue}
+            />
+          </div>
+          <div css={dataBoxStyle}>
+            <MDSTypography variant="title" size="m">
+              props
+            </MDSTypography>
+            <div>
+              <pre>{JSON.stringify(DEFAULT_VALUE, null, 2)}</pre>
+            </div>
+          </div>
+          <div css={dataBoxStyle}>
+            <MDSTypography variant="title" size="m">
+              결과
+            </MDSTypography>
+            <MDSTypography>선택한 날짜: {dayjs(selectedDate).format(DEFAULT_VALUE.format)}</MDSTypography>
+          </div>
         </div>
         <div>
-          <MDSInput
-            variant="textField"
-            value={inputValue}
-            custom={{
-              add: {
-                label: '선택',
-                onSubmit: submitInputChange,
-              },
-            }}
-            onChange={setInputValue}
+          <MDSCalendar
+            value={selectedDate}
+            onChange={handleChange}
+            minDate={dayjs(DEFAULT_VALUE.minDate, DEFAULT_VALUE.format).toDate()}
+            maxDate={dayjs(DEFAULT_VALUE.maxDate, DEFAULT_VALUE.format).toDate()}
           />
         </div>
-        <MDSCalendar value={selectedDate} onChange={handleChange} minDate={args.minDate} maxDate={args.maxDate} />
       </div>
     );
   },
@@ -111,100 +181,210 @@ export const DateRange: Story = {
   /**
    * todo - known issues
    * - [ ] start, end date에 같은 날짜를 선택 가능하는 기능
-   * - [ ] date range를 선택 중일 때, border style을 dashed로 하면 두 날짜 셀이 만나는 부분에서 dashed 선이 뭉치는 문제
    */
   render: function Render() {
-    const args = {
-      value: {
-        startDate: undefined,
-        endDate: undefined,
-      },
-    };
-    const [selectedDate, setSelectedDate] = useState<{ startDate?: Date; endDate?: Date }>(
-      args.value as { startDate?: Date; endDate?: Date }
-    );
-    const handleChange = (startDate: Date, endDate: Date) => {
-      setSelectedDate({ startDate, endDate });
+    const DEFAULT_VALUE = {
+      format: 'YYYY-MM-DD',
     };
 
-    const [inputStartDate, setInputStartDate] = useState<string>(selectedDate.startDate?.toLocaleDateString() || '');
-    const [inputEndDate, setInputEndDate] = useState<string>(selectedDate.endDate?.toLocaleDateString() || '');
-    const submitInputChange = (startDate: string, endDate: string) => {
-      const newStartDate = dayjs(startDate, 'MM/DD/YYYY').toDate();
-      const newEndDate = dayjs(endDate, 'MM/DD/YYYY').toDate();
-      setSelectedDate({ startDate: newStartDate, endDate: newEndDate });
-    };
+    const [injectedDate, setInjectedDate] = useState<{ start?: string; end?: string }>({});
+    const [dateInput, setDateInput] = useState<{ start?: string; end?: string }>({});
+    const [dateFromMDSDateInput, setDateFromMDSDateInput] = useState<{ start?: string; end?: string }>({});
 
     return (
-      <div>
-        <div>
-          <MDSTypography>
-            선택한 날짜 범위: {selectedDate.startDate?.toLocaleDateString()} -{' '}
-            {selectedDate.endDate?.toLocaleDateString()}
-          </MDSTypography>
+      <div css={testGroupStyle}>
+        <MDSTypography variant="title">Case 3: Date range</MDSTypography>
+        <div css={dataBoxLayout}>
+          <div css={dataBoxStyle}>
+            <MDSTypography variant="title" size="m">
+              외부 값 주입을 위한 컴포넌트
+            </MDSTypography>
+
+            <MDSInput
+              fullWidth
+              variant="textField"
+              value={dateInput.start || ''}
+              label="start date"
+              placeholder={DEFAULT_VALUE.format}
+              custom={{
+                add: {
+                  label: '완료',
+                  onSubmit: (value) => {
+                    setInjectedDate({ ...injectedDate, start: value });
+                  },
+                },
+              }}
+              onChange={(value) => {
+                setDateInput({ ...dateInput, start: value });
+              }}
+            />
+            <MDSInput
+              fullWidth
+              variant="textField"
+              value={dateInput.end || ''}
+              label="end date"
+              placeholder={DEFAULT_VALUE.format}
+              custom={{
+                add: {
+                  label: '완료',
+                  onSubmit: (value) => {
+                    setInjectedDate({ ...injectedDate, end: value });
+                  },
+                },
+              }}
+              onChange={(value) => {
+                setDateInput({ ...dateInput, end: value });
+              }}
+            />
+          </div>
+          <div css={dataBoxStyle}>
+            <MDSTypography variant="title" size="m">
+              props
+            </MDSTypography>
+            <div>
+              <pre>{JSON.stringify(DEFAULT_VALUE, null, 2)}</pre>
+            </div>
+          </div>
+          <div css={dataBoxStyle}>
+            <MDSTypography variant="title" size="m">
+              결과
+            </MDSTypography>
+            <MDSTypography>
+              선택한 start date:{' '}
+              {dateFromMDSDateInput.start
+                ? dayjs(dateFromMDSDateInput.start, DEFAULT_VALUE.format).format(DEFAULT_VALUE.format)
+                : ''}
+            </MDSTypography>
+            <MDSTypography>
+              선택한 end date:{' '}
+              {dateFromMDSDateInput.end
+                ? dayjs(dateFromMDSDateInput.end, DEFAULT_VALUE.format).format(DEFAULT_VALUE.format)
+                : ''}
+            </MDSTypography>
+          </div>
         </div>
-        <div
-          style={{ display: 'grid', gridTemplateColumns: '110px 4px 110px 60px', gap: '10px', alignItems: 'center' }}
-        >
-          <MDSInput variant="textField" value={inputStartDate} onChange={setInputStartDate} />
-          ~
-          <MDSInput variant="textField" value={inputEndDate} onChange={setInputEndDate} />
-          <MDSButton onClick={() => submitInputChange(inputStartDate, inputEndDate)}>선택</MDSButton>
-        </div>
-        <MDSCalendar value={selectedDate} onChange={handleChange} />
+        <MDSCalendar
+          value={{
+            startDate: injectedDate.start ? dayjs(injectedDate.start, DEFAULT_VALUE.format).toDate() : undefined,
+            endDate: injectedDate.end ? dayjs(injectedDate.end, DEFAULT_VALUE.format).toDate() : undefined,
+          }}
+          onChange={(startDate, endDate) => {
+            setDateFromMDSDateInput({
+              start: dayjs(startDate).format(DEFAULT_VALUE.format),
+              end: dayjs(endDate).format(DEFAULT_VALUE.format),
+            });
+          }}
+        />
       </div>
     );
   },
 };
+
 export const DateRangeWithMinMaxAndDefaultValue: Story = {
   /**
    * todo - known issues
    * - [ ] start, end date에 같은 날짜를 선택 가능하는 기능
-   * - [ ] date range를 선택 중일 때, border style을 dashed로 하면 두 날짜 셀이 만나는 부분에서 dashed 선이 뭉치는 문제
    */
   render: function Render() {
-    const args = {
-      value: {
-        startDate: new Date('2025-05-12'),
-        endDate: new Date('2025-05-20'),
-      },
-      minDate: new Date('2025-05-03'),
-      maxDate: new Date('2025-06-30'),
+    const DEFAULT_VALUE = {
+      format: 'YYYY-MM-DD',
+      minDate: '2023-11-22',
+      maxDate: '2027-11-22',
+      startDate: '2025-06-10',
+      endDate: '2025-06-20',
     };
-    const [selectedDate, setSelectedDate] = useState<{ startDate: Date; endDate: Date }>(
-      args.value as { startDate: Date; endDate: Date }
-    );
-    const handleChange = (startDate: Date, endDate: Date) => {
-      setSelectedDate({ startDate, endDate });
-    };
-
-    const [inputStartDate, setInputStartDate] = useState<string>(selectedDate.startDate.toLocaleDateString());
-    const [inputEndDate, setInputEndDate] = useState<string>(selectedDate.endDate.toLocaleDateString());
-    const submitInputChange = (startDate: string, endDate: string) => {
-      const newStartDate = dayjs(startDate, 'MM/DD/YYYY').toDate();
-      const newEndDate = dayjs(endDate, 'MM/DD/YYYY').toDate();
-      setSelectedDate({ startDate: newStartDate, endDate: newEndDate });
-    };
+    const [dateInput, setDateInput] = useState<{ start?: string; end?: string }>({});
+    const [dateFromMDSDateInput, setDateFromMDSDateInput] = useState<{ start: Date; end: Date }>({
+      start: dayjs(DEFAULT_VALUE.startDate, DEFAULT_VALUE.format).toDate(),
+      end: dayjs(DEFAULT_VALUE.endDate, DEFAULT_VALUE.format).toDate(),
+    });
 
     return (
-      <div>
-        <div>
-          <MDSTypography>minDate: {args.minDate.toLocaleDateString()}</MDSTypography>
-          <MDSTypography>maxDate: {args.maxDate.toLocaleDateString()}</MDSTypography>
-          <MDSTypography>
-            선택한 날짜 범위: {selectedDate.startDate.toLocaleDateString()} -{' '}
-            {selectedDate.endDate.toLocaleDateString()}
-          </MDSTypography>
+      <div css={testGroupStyle}>
+        <MDSTypography variant="title">Case 3: Date range</MDSTypography>
+        <div css={dataBoxLayout}>
+          <div css={dataBoxStyle}>
+            <MDSTypography variant="title" size="m">
+              외부 값 주입을 위한 컴포넌트
+            </MDSTypography>
+
+            <MDSInput
+              fullWidth
+              variant="textField"
+              value={dateInput.start || ''}
+              label="start date"
+              placeholder={DEFAULT_VALUE.format}
+              custom={{
+                add: {
+                  label: '완료',
+                  onSubmit: (value) => {
+                    setDateFromMDSDateInput((prev) => ({
+                      ...prev,
+                      start: dayjs(value, DEFAULT_VALUE.format).toDate(),
+                    }));
+                  },
+                },
+              }}
+              onChange={(value) => {
+                setDateInput({ ...dateInput, start: value });
+              }}
+            />
+            <MDSInput
+              fullWidth
+              variant="textField"
+              value={dateInput.end || ''}
+              label="end date"
+              placeholder={DEFAULT_VALUE.format}
+              custom={{
+                add: {
+                  label: '완료',
+                  onSubmit: (value) => {
+                    setDateFromMDSDateInput((prev) => ({
+                      ...prev,
+                      end: dayjs(value, DEFAULT_VALUE.format).toDate(),
+                    }));
+                  },
+                },
+              }}
+              onChange={(value) => {
+                setDateInput({ ...dateInput, end: value });
+              }}
+            />
+          </div>
+          <div css={dataBoxStyle}>
+            <MDSTypography variant="title" size="m">
+              props
+            </MDSTypography>
+            <div>
+              <pre>{JSON.stringify(DEFAULT_VALUE, null, 2)}</pre>
+            </div>
+          </div>
+          <div css={dataBoxStyle}>
+            <MDSTypography variant="title" size="m">
+              결과
+            </MDSTypography>
+            <MDSTypography>
+              선택한 start date: {dayjs(dateFromMDSDateInput.start, DEFAULT_VALUE.format).format(DEFAULT_VALUE.format)}
+            </MDSTypography>
+            <MDSTypography>
+              선택한 end date: {dayjs(dateFromMDSDateInput.end, DEFAULT_VALUE.format).format(DEFAULT_VALUE.format)}
+            </MDSTypography>
+          </div>
         </div>
-        <div
-          style={{ display: 'grid', gridTemplateColumns: '110px 4px 110px 60px', gap: '10px', alignItems: 'center' }}
-        >
-          <MDSInput variant="textField" value={inputStartDate} onChange={setInputStartDate} />
-          ~
-          <MDSInput variant="textField" value={inputEndDate} onChange={setInputEndDate} />
-          <MDSButton onClick={() => submitInputChange(inputStartDate, inputEndDate)}>선택</MDSButton>
-        </div>
-        <MDSCalendar value={selectedDate} minDate={args.minDate} maxDate={args.maxDate} onChange={handleChange} />
+        <MDSCalendar
+          value={{
+            startDate: dayjs(dateFromMDSDateInput.start, DEFAULT_VALUE.format).toDate(),
+            endDate: dayjs(dateFromMDSDateInput.end, DEFAULT_VALUE.format).toDate(),
+          }}
+          onChange={(startDate, endDate) => {
+            setDateFromMDSDateInput({
+              start: startDate,
+              end: endDate,
+            });
+          }}
+          minDate={dayjs(DEFAULT_VALUE.minDate, DEFAULT_VALUE.format).toDate()}
+          maxDate={dayjs(DEFAULT_VALUE.maxDate, DEFAULT_VALUE.format).toDate()}
+        />
       </div>
     );
   },

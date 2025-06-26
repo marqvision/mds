e21522ba7ecc2a7f78a49f94d034245524f4dd7b
jamie <jamie.cho@marqvision.com>
2025-06-26 15:44:54 +0900

    refactor(mds-v2): [PROD-10004] date range picker를 anchor의 종류별로 분리

--- @stories/components/organisms/DatePickers/DateRangePicker.stories.tsx
+++ @stories/components/organisms/DatePickers/DateRangePicker.stories.tsx
@@ -176,16 +176,114 @@ export const AnchorButton: Story = {
 
 export const AnchorPlainButton: Story = {
   render: function Render() {
+    const DEFAULT_VALUE = {
+      format: 'MM/DD/YYYY',
+      minDate: '06/02/2025', //'2025-06-02',
+      maxDate: '07/02/2025', //'2025-07-02',
+    };
+    const [selectedDate, setSelectedDate] = useState<{
+      startDate: Date | null;
+      endDate: Date | null;
+    }>({
+      startDate: null,
+      endDate: null,
+    });
+    const [tempDateRangeInput, setTempDateRangeInput] = useState<{ start: string; end: string }>({
+      start: '',
+      end: '',
+    });
+    const [dateFromMDSDateRangePicker, setDateFromMDSDateRangePicker] = useState<{ start: string; end: string }>({
+      start: '',
+      end: '',
+    });
+    const handleChange = (dates: { startDate: Date | null; endDate: Date | null }) => {
+      setSelectedDate(dates);
+      setDateFromMDSDateRangePicker({
+        start: dayjs(dates.startDate).format(DEFAULT_VALUE.format),
+        end: dayjs(dates.endDate).format(DEFAULT_VALUE.format),
+      });
+    };
+
     return (
       <div css={testGroupStyle}>
-        <MDSDateRangePicker
-          anchor={{
-            variant: 'plainButton',
-            props: {
-              children: '🚧 In progress',
-            },
-          }}
-        />
+        <div css={dataBoxLayout}>
+          <div css={dataBoxStyle}>
+            <MDSTypography variant="title" size="m">
+              외부 값 주입을 위한 컴포넌트
+            </MDSTypography>
+            <MDSInput
+              fullWidth
+              value={tempDateRangeInput.start}
+              label="start date"
+              placeholder={DEFAULT_VALUE.format}
+              custom={{
+                add: {
+                  label: '완료',
+                  onSubmit: (value) => {
+                    setSelectedDate({ ...selectedDate, startDate: dayjs(value).toDate() });
+                  },
+                },
+              }}
+              onChange={(value) => {
+                setTempDateRangeInput({ ...tempDateRangeInput, start: value });
+              }}
+            />
+            <MDSInput
+              fullWidth
+              value={tempDateRangeInput.end}
+              label="end date"
+              placeholder={DEFAULT_VALUE.format}
+              custom={{
+                add: {
+                  label: '완료',
+                  onSubmit: (value) => {
+                    setSelectedDate({ ...selectedDate, endDate: dayjs(value).toDate() });
+                  },
+                },
+              }}
+              onChange={(value) => {
+                setTempDateRangeInput({ ...tempDateRangeInput, end: value });
+              }}
+            />
+          </div>
+          <div css={dataBoxStyle}>
+            <MDSTypography variant="title" size="m">
+              props
+            </MDSTypography>
+            <div>
+              <pre>{JSON.stringify(DEFAULT_VALUE, null, 2)}</pre>
+            </div>
+          </div>
+          <div css={dataBoxStyle}>
+            <MDSTypography variant="title" size="m">
+              결과
+            </MDSTypography>
+            <MDSTypography>startDate: {dateFromMDSDateRangePicker.start}</MDSTypography>
+            <MDSTypography>endDate: {dateFromMDSDateRangePicker.end}</MDSTypography>
+          </div>
+        </div>
+        <div>
+          <MDSDateRangePicker
+            anchor={{
+              variant: 'plainButton',
+              format: 'MMM DD, YYYY',
+              separator: ' ~ ',
+              props: {
+                children: 'Button',
+              },
+            }}
+            startDate={{
+              value: selectedDate.startDate ? dayjs(selectedDate.startDate).format(DEFAULT_VALUE.format) : undefined,
+            }}
+            endDate={{
+              value: selectedDate.endDate ? dayjs(selectedDate.endDate).format(DEFAULT_VALUE.format) : undefined,
+            }}
+            format={DEFAULT_VALUE.format as MDSDateRangePickerProps['format']}
+            minDate={dayjs(DEFAULT_VALUE.minDate).toDate()}
+            maxDate={dayjs(DEFAULT_VALUE.maxDate).toDate()}
+            onChange={handleChange}
+          />
+        </div>
       </div>
     );
   },
---
+++ components/organisms/DatePickers/DateRangePicker/DateRangePickerCore.tsx
@@ -0,0 +1,157 @@
+import { useCallback, useRef, useState } from 'react';
+import dayjs from 'dayjs';
+import styled from '@emotion/styled';
+import { MDSDateInputGroup } from '../DateInputGroup';
+import { MDSCalendar } from '../Calendar';
+import { MDSDivider } from '../../../atoms/Divider';
+import { MDSPlainButton } from '../../../molecules/PlainButton';
+import { MDSButton } from '../../../molecules/Button';
+import { DEFAULT_PROPS } from '../@constants';
+import { DateValidationError } from '../@types';
+import { validateDateRange } from '../@utils';
+import { DateRangePickerProps } from './@types';
+
+const DateRangePickerContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 304px;
+  background-color: ${({ theme }) => theme.color.bg.fill.neutral.default.normal};
+`;
+const DateRangePickerLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  .mds-date-picker-input-container {
+    width: 100%;
+    padding: 12px 12px 0;
+  }
+`;
+const DateRangePickerActionContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  gap: 16px;
+  padding: 12px;
+`;
+
+export const DateRangePickerCore = (props: DateRangePickerProps) => {
+  const { startDate, endDate, format = DEFAULT_PROPS.format, onChange, minDate, maxDate, anchor, onClose } = props;
+
+  const [store, setStore] = useState<
+    | {
+        startDate: Date;
+        endDate: Date;
+      }
+    | undefined
+  >(
+    startDate?.value && endDate?.value
+      ? {
+          startDate: dayjs(startDate.value, format).toDate(),
+          endDate: dayjs(endDate.value, format).toDate(),
+        }
+      : undefined
+  );
+  const frozenOnChange = useRef(onChange);
+
+  const handleDateInputGroupChange = useCallback(
+    (dates: { startDate: Date | null; endDate: Date | null }) => {
+      setStore(
+        dates.startDate && dates.endDate
+          ? {
+              startDate: dayjs(dates.startDate, format).toDate(),
+              endDate: dayjs(dates.endDate, format).toDate(),
+            }
+          : undefined
+      );
+    },
+    [format]
+  );
+
+  const handleCalendarChange = useCallback((startDate: Date, endDate: Date) => {
+    setStore({
+      startDate,
+      endDate,
+    });
+  }, []);
+
+  const handleApply = () => {
+    if (frozenOnChange.current) {
+      frozenOnChange.current({
+        startDate: store?.startDate ?? null,
+        endDate: store?.endDate ?? null,
+      });
+    }
+  };
+
+  const handleDateInputGroupError = (error: DateValidationError) => {
+    console.log('>>>>>> error', error);
+  };
+
+  const handleCalendarError = (error: DateValidationError) => {
+    console.log('>>>>>> error', error);
+  };
+
+  const isReadyToApply = validateDateRange({
+    startDate: store?.startDate ?? null,
+    endDate: store?.endDate ?? null,
+    format,
+    minDate,
+    maxDate,
+  });
+
+  return (
+    <DateRangePickerContainer>
+      <DateRangePickerLayout>
+        {anchor.variant !== 'input' && (
+          <div className="mds-date-picker-input-container">
+            <MDSDateInputGroup
+              format={format}
+              startDate={{
+                value: store?.startDate ? dayjs(store?.startDate).format(format) : undefined,
+              }}
+              endDate={{
+                value: store?.endDate ? dayjs(store?.endDate).format(format) : undefined,
+              }}
+              onDateChange={handleDateInputGroupChange}
+              onError={handleDateInputGroupError}
+              minDate={minDate}
+              maxDate={maxDate}
+            />
+          </div>
+        )}
+        <MDSCalendar
+          value={{
+            startDate: store?.startDate,
+            endDate: store?.endDate,
+          }}
+          onChange={handleCalendarChange}
+          onError={handleCalendarError}
+          minDate={minDate}
+          maxDate={maxDate}
+        />
+      </DateRangePickerLayout>
+
+      <MDSDivider />
+
+      <DateRangePickerActionContainer>
+        <MDSPlainButton
+          color="bluegray"
+          onClick={() => {
+            onClose?.();
+          }}
+        >
+          Cancel
+        </MDSPlainButton>
+        <MDSButton
+          isDisabled={!isReadyToApply}
+          onClick={() => {
+            handleApply();
+            onClose?.();
+          }}
+        >
+          Apply
+        </MDSButton>
+      </DateRangePickerActionContainer>
+    </DateRangePickerContainer>
+  );
+};

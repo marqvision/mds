67f7b6947557b64a4b26ac6503feb8666e09e74a
jamie <jamie.cho@marqvision.com>
2025-07-29 14:18:09 +0900

    chore(mds-v2): [PROD-16104] 사용하지 않는 prop 삭제

--- components/organisms/DownloadPanel/index.tsx
+++ components/organisms/DownloadPanel/index.tsx
@@ -39,7 +39,6 @@ export const DownloadPanel = (props: DownloadPanelProps) => {
           />
           <MDSPlainButton
             color="bluegray"
-            // size="small"
             icon={<MDSIcon.CloseDelete variant="outline" />}
             onClick={panel.onClose}
           />

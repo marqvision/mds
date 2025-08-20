7d9754a8b62c938815ae16d0ab90fc547b6fe43c
Eddie <eddie.jang@marqvision.com>
2025-08-20 14:47:22 +0900

    feat(mds): add small variant, purple and teal color

--- components/molecules/MessageBox/@constants/index.ts
+++ components/molecules/MessageBox/@constants/index.ts
@@ -1,22 +1,82 @@
-import { MessageBoxContentColorMap } from '../@types';
+import { ImageSizesType, MessageBoxContentColorMap } from '../@types';
 
 export const MessageBoxContentColor: MessageBoxContentColorMap = {
-  neutral: {
+  white: {
     mainColor: 'color/content/neutral/default/normal',
   },
-  default: {
+  bluegray: {
     mainColor: 'color/content/neutral/default/normal',
   },
-  primary: {
+  blue: {
     mainColor: 'color/content/neutral/default/normal',
   },
-  success: {
+  green: {
     mainColor: 'color/content/success/default/normal',
   },
-  warning: {
+  yellow: {
     mainColor: 'color/content/warning/default/normal',
   },
-  critical: {
+  red: {
     mainColor: 'color/content/critical/default/normal',
+  },
+  purple: {
+    mainColor: 'color/content/purple/default/normal',
+  },
+  teal: {
+    mainColor: 'color/content/teal/default/normal',
+  },
+} as const;
+
+export const IconSizes = {
+  default: {
+    info: 20,
+    close: 20,
+  },
+  small: {
+    info: 18,
+    close: 16,
+  },
+} as const;
+
+export const TypographySizes = {
+  default: {
+    title: {
+      fontSize: 'l',
+    },
+    message: {
+      fontSize: 'm',
+    },
+  },
+  small: {
+    title: {
+      fontSize: 'm',
+    },
+    message: {
+      fontSize: 's',
+    },
+  },
+} as const;
+
+export const ButtonSizes = {
+  default: {
+    titleCTA: 'medium',
+    messageCTA: 'medium',
+    action: 'large',
+  },
+  small: {
+    titleCTA: 'small',
+    messageCTA: 'small',
+    action: 'medium',
+  },
+} as const;
+
+export const ImageSizes: ImageSizesType = {
+  default: {
+    big: '66px',
+    small: '32px',
+  },
+  small: {
+    big: '58px',
+    small: '28px',
   },
 } as const;

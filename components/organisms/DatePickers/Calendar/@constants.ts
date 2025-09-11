ef354679d06e520959d41f369fdf68c4bb68353c
jamie <jamie.cho@marqvision.com>
2025-09-11 13:07:10 +0900

    fix(mds-v2): [PROD-16890] calendar에서 선택 가능한 년도의 범위르 전후 40년으로 확대

--- components/organisms/DatePickers/Calendar/@constants.ts
+++ components/organisms/DatePickers/Calendar/@constants.ts
@@ -1,7 +1,7 @@
 import dayjs from 'dayjs';
 
 export const WEEKDAYS = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
-export const AVAILABLE_YEARS = Array.from({ length: 10 }, (_, i) => dayjs().add(i - 5, 'year')); // todo-@jamie: 스펙에 따라 전/후년도 기간 업데이트하기
+export const AVAILABLE_YEARS = Array.from({ length: 80 }, (_, i) => dayjs().add(i - 40, 'year')); // todo-@jamie: 스펙에 따라 전/후년도 기간 업데이트하기
 export const MONTH_LABELS = [
   'January',
   'February',

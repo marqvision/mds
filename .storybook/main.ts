137ef541ae9eb245ecea91cb3272d6f6c57d81bf
jamie <jamie.cho@marqvision.com>
2025-12-12 10:51:11 +0900

    fix(mds|mds-v2|shared): [prod-18407] glob 패키지 보안 취약점을 해결하기 위해 storybook 버전 업그레이드

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,35 +1,23 @@
-import { join, dirname } from 'path';
 import type { StorybookConfig } from '@storybook/react-vite';
 import react from '@vitejs/plugin-react';
 import checker from 'vite-plugin-checker';
-import path from 'path';
-
-/**
- * This function is used to resolve the absolute path of a package.
- * It is needed in projects that use Yarn PnP or are set up within a monorepo.
- */
-function getAbsolutePath(value: string): string {
-  return dirname(require.resolve(join(value, 'package.json')));
-}
+import { fileURLToPath } from "node:url";
 
 const config: StorybookConfig = {
   stories: ['../@stories/**/*.stories.@(js|jsx|mjs|ts|tsx|mdx)'],
   staticDirs: ['../foundation'],
-  addons: [
-    getAbsolutePath('@storybook/addon-docs'),
-    getAbsolutePath('@storybook/addon-links'),
-  ],
+  addons: ['@storybook/addon-docs', '@storybook/addon-links'],
   framework: {
-    name: getAbsolutePath('@storybook/react-vite'),
+    name: '@storybook/react-vite',
     options: {},
   },
 
   async viteFinal(config) {
-    config.plugins.push(
+    config.plugins?.push(
       checker({
         typescript: {
-          tsconfigPath: path.resolve(__dirname, '../tsconfig.json'),
-        }
+          tsconfigPath: fileURLToPath(import.meta.resolve('../tsconfig.json')),
+        },
       }),
       react({
         jsxImportSource: '@emotion/react',

f4984cf21f8ab55d9ef3391ae1bf7fe37b46ee0a
PooLyum Lim <chinapoopoo@gmail.com>
2025-12-15 17:08:17 +0900

    feat(mds-v2): [PROD-18514] date picker format에 M.D 추가

--- components/organisms/DatePickers/@constants.ts
+++ components/organisms/DatePickers/@constants.ts
@@ -5,12 +5,14 @@ export const DATE_SHAPE_REGEX_MAP: Record<AvailableDateFormat, RegExp> = {
   'MM/DD/YYYY': /^(?!.*\/\/)\d{0,2}(\/(\d{0,2}(\/(\d{0,4})?)?)?)?$/,
   'YYYY-MM-DD': /^(?!.*--)\d{0,4}(-(\d{0,2}(-(\d{0,2})?)?)?)?$/,
   'MMM DD, YYYY': /^(?!.*,,\s*)\w{0,3}(\s+\d{0,2}(\s*,\s*(\d{0,4})?)?)?$/,
+  'M.D': /^(?!.*\.\.)\d{0,2}(\.\d{0,2})?$/,
 };
 
 export const SEPARATOR_MAP: Record<AvailableDateFormat, string> = {
   'MM/DD/YYYY': '/',
   'YYYY-MM-DD': '-',
   'MMM DD, YYYY': ' ',
+  'M.D': '.',
 };
 
 export const DEFAULT_PROPS = {
--- components/organisms/DatePickers/@types.ts
+++ components/organisms/DatePickers/@types.ts
@@ -2,4 +2,5 @@ export type DateValidationError = 'INVALID_DATE' | 'MIN_DATE' | 'MAX_DATE';
 export type AvailableDateFormat =
   | 'MM/DD/YYYY' // 06/26/2025
   | 'YYYY-MM-DD' // 2025-06-26
-  | 'MMM DD, YYYY'; // Jan 01, 2020
+  | 'MMM DD, YYYY' // Jan 01, 2020
+  | 'M.D'; // 6.26
